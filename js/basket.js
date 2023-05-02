04d46030c19932d7d2d1fbe807ac9b6101d21947
Vugarismayil <vugarai@code.edu.az>
2023-05-02 20:59:00 +0400

    Add:Basket

---
+++ js/basket.js
@@ -0,0 +1,94 @@
+let productList = JSON.parse(localStorage.getItem("products"));
+
+if (productList.length === 0) {
+  document.querySelector("table").classList.add("d-none");
+  document.querySelector(".message").classList.remove("d-none");
+} else {
+  document.querySelector("table").classList.remove("d-none");
+  document.querySelector(".message").classList.add("d-none");
+}
+
+function ShowProduct() {
+  let productList = JSON.parse(localStorage.getItem("products"));
+
+  let x = "";
+  productList.forEach((element) => {
+    x += `
+   <tr data-id="${element.productId}" >
+   <td> <span id="dlt">x</span> </td>
+   <td> <img src="${element.image}" alt=""></td>
+   <td>${element.productName}</td>
+   <td>${element.productPrice}</td>
+   <td><input id="${element.productId}" type="number" value="${
+      element.productCount
+    }" onchange="handleCountChange('${element.productId}', this.value)"></td>
+   <td> ${element.productPrice.split(" ")[0] * element.productCount} ₼  </td>
+</tr>
+   `;
+  });
+
+  document.querySelector("tbody").innerHTML = x;
+
+  DeleteItem();
+}
+
+ShowProduct();
+
+function DeleteItem() {
+  let productList = JSON.parse(localStorage.getItem("products"));
+
+  let deleteElement = document.querySelectorAll("#dlt");
+
+  for (let dltElement of deleteElement) {
+    dltElement.addEventListener("click", function () {
+      let dataId = this.parentElement.parentElement.getAttribute("data-id");
+      let existPr = productList.find((pr) => pr.productId === dataId);
+      let index = productList.indexOf(existPr);
+      productList.splice(index, 1);
+      localStorage.setItem("products", JSON.stringify(productList));
+
+      if (existPr.productCount >= 0) {
+        this.parentElement.parentElement.remove();
+      }
+
+      if (productList.length === 0) {
+        document.querySelector("table").classList.add("d-none");
+        document.querySelector(".message").classList.remove("d-none");
+      } else {
+        document.querySelector("table").classList.remove("d-none");
+        document.querySelector(".message").classList.add("d-none");
+      }
+
+      document.querySelector("#count").innerHTML = productList.length;
+    });
+  }
+  ShowCountProduct();
+}
+
+function handleCountChange(productId, newCount) {
+  let productList = JSON.parse(localStorage.getItem("products"));
+  if (newCount >= 0) {
+    productList.forEach((element) => {
+      if (element.productId === productId) {
+        element.productCount = newCount;
+        let productPrice = parseFloat(element.productPrice.split(" ")[0]);
+        let totalPrice = productPrice * newCount;
+        document.getElementById(
+          element.productId
+        ).parentNode.nextElementSibling.innerHTML = totalPrice + " AZN";
+      }
+    });
+    localStorage.setItem("products", JSON.stringify(productList));
+  } else {
+    alert("Minimum deyer 0 ola biler");
+  }
+  DeleteItem();
+  ShowProduct();
+}
+
+function ShowCountProduct() {
+  let product_list = JSON.parse(localStorage.getItem("products"));
+  document.querySelector("#count").innerHTML = product_list.length;
+}
+
+ShowCountProduct();
--- js/main.js
+++ js/main.js
@@ -1,53 +1,98 @@
 $(document).ready(function () {
-    $('.currency').click(function (e) {
-        e.preventDefault();
-        var selected = $(this).html();
-        $('.dropdown-toggle.from').html(selected);
-    });
+  $(".currency").click(function (e) {
+    e.preventDefault();
+    var selected = $(this).html();
+    $(".dropdown-toggle.from").html(selected);
+  });
 });
 
 $(document).ready(function () {
-    $('.currencyto').click(function (e) {
-        e.preventDefault();
-        var selected = $(this).html();
-        $('.dropdown-toggle.to').html(selected);
-    });
+  $(".currencyto").click(function (e) {
+    e.preventDefault();
+    var selected = $(this).html();
+    $(".dropdown-toggle.to").html(selected);
+  });
 });
 
 function myFunction() {
-    window.open("/html/InternetBanking.html");
+  window.open("/html/InternetBanking.html");
 }
 
-$('#night-theme').click(function () {
-    $('#display_advance').toggle('100');
-    $("i", this).toggleClass("bi bi-moon-stars bi bi-brightness-high");
-
+$("#night-theme").click(function () {
+  $("#display_advance").toggle("100");
+  $("i", this).toggleClass("bi bi-moon-stars bi bi-brightness-high");
 });
 
 $(document).ready(function () {
-    $("#night-theme").click(function () {
-        $("#night-theme").css("background-color", "white");
-        $("#night-theme").css("color", "black");
+  $("#night-theme").click(function () {
+    $("#night-theme").css("background-color", "white");
+    $("#night-theme").css("color", "black");
 
-        $("body,header").css("background-color", "black");
-        $("body,b,a,.cl").css("color", "white");
-        $(".bc").css("background-color", "#1A1A1A");
-        $("#Valyuta-calculator,.bc2").css("background-color", "#313131");
-        $("#input,#input2").css("background-color", "#464646");
-    });
+    $("body,header").css("background-color", "black");
+    $("body,b,a,.cl").css("color", "white");
+    $(".bc").css("background-color", "#1A1A1A");
+    $("#Valyuta-calculator,.bc2").css("background-color", "#313131");
+    $("#input,#input2").css("background-color", "#464646");
+  });
 });
 
+// let ferdibtn = document.querySelector("#button-ferdi");
+// let biznesbtn = document.querySelector("#button-biznes");
+// biznesbtn.onclick = function () {
+//   biznesbtn.classList.add("change");
+//   ferdibtn.classList.add("default");
+//   biznesbtn.classList.remove("default");
+// };
+// ferdibtn.onclick = function () {
+//   ferdibtn.classList.remove("default");
+//   biznesbtn.classList.add("default");
+// };
 
-let ferdibtn = document.querySelector('#button-ferdi')
-let biznesbtn = document.querySelector('#button-biznes')
-biznesbtn.onclick= function(){
-biznesbtn.classList.add('change');
-ferdibtn.classList.add('default');
-biznesbtn.classList.remove('default')
+if (localStorage.getItem("products") === null) {
+  localStorage.setItem("products", JSON.stringify([]));
 }
-ferdibtn.onclick = function(){
-    ferdibtn.classList.remove('default');
-    biznesbtn.classList.add('default');
-  
+
+let addButtons = document.querySelectorAll("#addBaket");
+
+for (let btn of addButtons) {
+  btn.addEventListener("click", function (e) {
+    e.preventDefault();
+    let productString = localStorage.getItem("products");
+    let productList = JSON.parse(productString);
+    console.log("sagol");
+    let src = this.parentElement.previousElementSibling.children[0].src;
+    console.log(src);
+    let name =
+      this.previousElementSibling.previousElementSibling.children[0].innerHTML;
+    console.log(name);
+    let type = this.previousElementSibling.children[0].innerHTML;
+    console.log(type);
+    let price = this.previousElementSibling.children[2].innerHTML;
+    console.log(price);
+
+    let id =
+      this.parentElement.parentElement.parentElement.getAttribute("data-id");
+
+    let product = {
+      productId: id,
+      image: src,
+      productName: name,
+      productType: type,
+      productPrice: price,
+      productCount: 1,
+    };
+
+    let existPr = productList.find((pr) => pr.productId === id);
+
+    if (existPr === undefined) {
+      productList.push(product);
+    } else {
+      existPr.productCount = Number(existPr.productCount)+1;
+    }
+
+    localStorage.setItem("products", JSON.stringify(productList));
+
+  });
 }
+
 
